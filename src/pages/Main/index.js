baeba356e5d922d0cc7f05d6457d699c1bf38133
Euller Cristian <eullercdr@gmail.com>
2020-02-17 15:52:19 -0300

    Estilizando component Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+        <SubmitButton disable>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
