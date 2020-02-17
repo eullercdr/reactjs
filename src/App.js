b8e686d556203364d45e6cca3db9ec024bca0f6c
Euller Cristian <eullercdr@gmail.com>
2020-02-17 15:31:29 -0300

    Criando styles globalmente

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />;
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
