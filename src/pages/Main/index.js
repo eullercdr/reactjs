172d792c172f55792ec244158bb080a703630878
Euller Cristian <eullercdr@gmail.com>
2020-02-18 19:11:18 -0300

    Utilizando navegação com Link e passando parametros

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
@@ -82,7 +83,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="#">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
